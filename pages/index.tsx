eb62bbea247da5561b5e9e9fbe696e32c2f31150
Rodrigo Filipe Pereira Lourenço <rodrigofplourenco2002@gmail.com>
2022-01-24 22:16:27 +0000

    Fixed grammar errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { fadeInUp, stagger, routeAnimation } from '../animations';
 export default function Home() {
   return (
     <motion.div className='flex flex-col flex-grow px-6 pt-1' variants={routeAnimation} initial="initial" animate="animate" exit="exit">
-      <h5 className='my-3 font-medium'>I describe my self as someone who s persistent, a quick learner and loves problem solving by using simple, efficient and scalable solutions.</h5>
+      <h5 className='my-3 font-medium'>I describe my self as someone who is persistent, a quick learner and loves problem solving by using simple, efficient and scalable solutions.</h5>
 
       <div className='flex-grow p-4 mt-5 bg-gray-200 dark:bg-dark-100' style={{ margin: '0 -1.5rem' }}>
         <h6 className='my-3 text-xl font-bold tracking-wide'>What I Offer</h6>
